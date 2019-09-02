eececd604bbc45a4b103fa8d8ad274868a42fafa
Erik Castricum <git@cas-online.nl>
2019-09-02 14:48:49 +0200

    tone: remove WfdCommon
    not building lineage sources
    
    Signed-off-by: Erik Castricum <git@cas-online.nl>
    Change-Id: I08e8895ce917c1462243bb6c9ec543daf8939a8e

--- tone.mk
+++ tone.mk
@@ -374,7 +374,3 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/fstman.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/fstman.ini \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
-
-# WiFi Display
-PRODUCT_BOOT_JARS += \
-    WfdCommon
