b71c859f48cbe21ecf76b63728555963739662a6
Erik Castricum <git@cas-online.nl>
2019-09-02 15:07:39 +0200

    [DONOTMERGE]tone: add twrp
    todo: fix predecrypt
    
    Signed-off-by: Erik Castricum <git@cas-online.nl>
    Change-Id: I2959ddc09b49bab7d8a101118a7cf601f370459b

--- tone.mk
+++ tone.mk
@@ -374,3 +374,5 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/fstman.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/fstman.ini \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+$(call inherit-product, device/sony/tone-common/recovery/twrp.mk)
