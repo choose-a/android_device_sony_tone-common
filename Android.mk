8f3ae3ea2466f9103ef7fe0c9a497a410fad42a2
Erik Castricum <git@cas-online.nl>
2019-09-07 16:34:20 +0200

    tone: fix including makefiles
    Signed-off-by: Erik Castricum <git@cas-online.nl>
    Change-Id: I56dccd887dddbf06c40ce28af0ebc26b9db5e805

--- Android.mk
+++ Android.mk
@@ -25,7 +25,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(filter-out kagura kagura_ds,$(TARGET_DEVICE)),)
+ifneq ($(filter kagura kagura_ds,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
