c918f9add10f97081a17dec013a9ac6e023e1706
Erik Castricum <git@cas-online.nl>
2019-09-11 13:55:42 +0200

    tone: disable verity
    Signed-off-by: Erik Castricum <git@cas-online.nl>
    Change-Id: I672d8f2670761d3ef6c4b48eae99825e368a8a85

--- tone.mk
+++ tone.mk
@@ -350,9 +350,9 @@ PRODUCT_PACKAGES += \
     vndk-sp
 
 # Verity
-PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/c0c4000.sdhci/by-name/system
-PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/soc/c0c4000.sdhci/by-name/vendor
-$(call inherit-product, build/target/product/verity.mk)
+#PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/c0c4000.sdhci/by-name/system
+#PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/soc/c0c4000.sdhci/by-name/vendor
+#$(call inherit-product, build/target/product/verity.mk)
 
 # Vibrator
 PRODUCT_PACKAGES += \
