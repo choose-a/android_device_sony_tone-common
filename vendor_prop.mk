800e164c04554cc34feae8ea30e72e091de3051b
Erik Castricum <git@cas-online.nl>
2019-09-16 09:10:04 +0200

    vendor prop update
    Signed-off-by: Erik Castricum <git@cas-online.nl>
    Change-Id: I347bb10bec1649efba35f83bb6bce8d6f829f646

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,120 +1,61 @@
-#
-# Copyright (C) 2018 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
+## Audio
 PRODUCT_PROPERTY_OVERRIDES += \
+    audio.deep_buffer.media=true \
+    audio.offload.video=false \
     af.fast_track_multiplier=1 \
-    vendor.audio_hal.period_size=192 \
+    vendor.audio_hal.period_size=240 \
+    ro.qc.sdk.audio.fluencetype=none \
+    ro.qc.sdk.audio.ssr=false \
+    persist.audio.dualmic.config=endfire \
     ro.vendor.audio.sdk.fluencetype=none \
     persist.vendor.audio.fluence.voicecall=true \
     persist.vendor.audio.fluence.voicerec=false \
     persist.vendor.audio.fluence.speaker=true \
     vendor.audio.tunnel.encode=false \
     vendor.audio.offload.buffer.size.kb=64 \
-    audio.offload.video=false \
+    vendor.audio.offload.pcm.16bit.enable=false \
+    vendor.audio.offload.pcm.24bit.enable=false \
     vendor.audio.offload.track.enable=true \
-    audio.deep_buffer.media=true \
-    vendor.voice.path.for.pcm.voip=true \
+    vendor.voice.path.for.pcm.voip=false \
     vendor.audio.offload.multiaac.enable=true \
-    vendor.audio.dolby.ds2.enabled=false \
-    vendor.audio.dolby.ds2.hardbypass=false \
-    vendor.audio.offload.multiple.enabled=true \
+    vendor.audio.offload.multiple.enabled=false \
+    vendor.audio.offload.min.duration.secs=60 \
     vendor.audio.offload.passthrough=false \
     ro.vendor.audio.sdk.ssr=false \
-    vendor.audio.offload.gapless.enabled=true \
+    vendor.audio.offload.gapless.enabled=false \
     vendor.audio.safx.pbe.enabled=true \
     vendor.audio.parser.ip.buffer.size=262144 \
-    vendor.audio.hw.aac.encoder=true \
+    vendor.audio.hw.aac.encoder=false \
     vendor.audio.use.sw.alac.decoder=true \
     vendor.audio.use.sw.ape.decoder=true \
     vendor.audio.flac.sw.decoder.24bit=true \
     persist.vendor.bt.enable.splita2dp=false \
+    persist.vendor.audio.speaker.prot.enable=true \
+    persist.vendor.audio.spkr.cal.duration=5 \
+    audio.offload.disable=false \
+    audio.playback.mch.downsample=0 \
+    persist.vendor.audio.fluence.mode=none \
+    persist.vendor.audio.endcall.delay=250 \
+    persist.vendor.audio.fluence.voicecomm=true
+
+## Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
     qcom.bluetooth.soc=rome \
     ro.bluetooth.emb_wp_mode=true \
     ro.bluetooth.wipower=true \
-    camera.disable_zsl_mode=1 \
-    Camera.no_navigation_bar=true \
-    vendor.media.vpp.enable=true \
-    vendor.vidc.enc.disable.pq=true \
-    media.stagefright.enable-player=true \
-    media.stagefright.enable-http=true \
-    media.stagefright.enable-aac=true \
-    media.stagefright.enable-qcp=true \
-    media.stagefright.enable-scan=true \
-    mmp.enable.3g2=true \
-    media.aac_51_output_enabled=true \
-    mm.enable.smoothstreaming=true \
-    vidc.enc.dcvs.extra-buff-count=2 \
-    vendor.vidc.enc.disable.pq=true \
-    mm.enable.qcom_parser=760765 \
-    persist.mm.enable.prefetch=true \
-    ro.vendor.qti.config.zram=true \
-    persist.cne.rat.wlan.chip.oem=nqc \
-    persist.cne.feature=1 \
-    sdm.debug.disable_rotator_split=1 \
-    ro.opengles.version=196610 \
-    debug.sf.enable_hwc_vds=1 \
-    debug.sf.hw=1 \
-    debug.sf.latch_unsignaled=1 \
-    debug.egl.hw=1 \
-    debug.gralloc.enable_fb_ubwc=1 \
-    ro.hwui.texture_cache_flushrate=0.5 \
-    ro.sf.lcd_density=480 \
-    ro.vendor.extension_library=libqti-perfd-client.so \
-    ro.vendor.gt_library=libqti-gt.so \
-    ro.vendor.at_library=libqti-at.so \
-    sys.games.gt.prof=1 \
-    ro.com.google.clientidbase=android-sonymobile  \
-    ro.com.google.clientidbase.ms=android-sonymobile \
-    persist.vendor.ims.vcel_rtcp_report=5 \
-    persist.ims.disableUserAgent=1 \
-    persist.vendor.radio.add_power_save=1 \
-    persist.vendor.radio.block_allow_data=1 \
-    persist.vendor.radio.wait_for_pbm=1 \
-    persist.vendor.radio.mt_sms_ack=19 \
-    persist.vendor.radio.oem_socket=true \
-    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-    persist.rild.nitz_plmn="" \
-    persist.rild.nitz_long_ons_0=""\
-    persist.rild.nitz_long_ons_1="" \
-    persist.rild.nitz_long_ons_2="" \
-    persist.rild.nitz_long_ons_3="" \
-    persist.rild.nitz_short_ons_0="" \
-    persist.rild.nitz_short_ons_1="" \
-    persist.rild.nitz_short_ons_2="" \
-    persist.rild.nitz_short_ons_3="" \
-    ril.subscription.types=NV,RUIM \
-    DEVICE_PROVISIONED=1 \
-    ro.nfc.port=I2C \
-    ro.qti.sensors.dev_ori=false \
-    ro.qti.sensors.pmd=false \
-    ro.qti.sensors.sta_detect=false \
-    ro.qti.sensors.mot_detect=false \
-    ro.use_data_netmgrd=true \
-    persist.data.netmgrd.qos.enable=true \
-    persist.data.mode=concurrent \
-    persist.rmnet.data.enable=true \
-    persist.data.wda.enable=true \
-    persist.data.df.dl_mode=5 \
-    persist.data.df.ul_mode=5 \
-    persist.data.df.agg.dl_pkt=10 \
-    persist.data.df.agg.dl_size=4096 \
-    persist.data.df.mux_count=8 \
-    persist.data.df.iwlan_mux=9 \
-    persist.data.df.dev_name=rmnet_usb0 \
-    ro.control_privapp_permissions=enforce \
+    ro.bt.bdaddr_path=/data/vendor/bluetooth/bluetooth_bdaddr
+
+## Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.camera.HAL3.enabled=1 \
+    persist.camera.debug.logfile=0 \
+    persist.camera.gyro.disable=0 \
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam,org.lineageos.snap \
+    persist.camera.camera2=true \
+    camera.disable_zsl_mode=1
+
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=8m \
     dalvik.vm.heapgrowthlimit=256m \
     dalvik.vm.heapsize=512m \
@@ -123,4 +64,95 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapmaxfree=16m \
     dalvik.vm.dex2oat-threads=2 \
     dalvik.vm.image-dex2oat-threads=4 \
+
+# Data modules
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.data.iwlan.enable=true \
+    persist.data.mode=concurrent \
+    persist.data.netmgrd.qos.enable=true \
+    ro.use_data_netmgrd=true
+
+## CNE
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.cne.rat.wlan.chip.oem=nqc \
+    persist.cne.feature=1 \
+    persist.vendor.cne.feature=1
+
+# DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true
+
+## Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.gralloc.enable_fb_ubwc=1 \
+    debug.gralloc.enable_fb_ubwc=1 \
+    vendor.display.disable_skip_validate=1 \
+    sdm.debug.disable_skip_validate=1 \
+    dev.pm.dyn_samplingrate=1 \
+    persist.demo.hdmirotationlock=false \
+    persist.hwc.enable_vds=1 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.opengles.version=196610 \
+    debug.sf.recomputecrop=0 \
+    ro.sf.lcd_density=480
+
+# IMS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1
+
+## Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.aac_51_output_enabled=true \
+    mm.enable.qcom_parser=760765 \
+    vendor.mm.en.sec.smoothstreaming=false \
+    mm.enable.smoothstreaming=false \
+    mmp.enable.3g2=true \
+    vidc.enc.dcvs.extra-buff-count=2 \
+    vendor.vidc.enc.disable.pq=true
+
+# NFC
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.nfc.port=I2C
+
+# Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=libqti-perfd-client.so
+
+## Play Store
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-sonymobile  \
+    ro.com.google.clientidbase.ms=android-sonymobile
+
+## QCOM
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    DEVICE_PROVISIONED=1 \
+    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+    ril.subscription.types=NV,RUIM \
+    persist.radio.apm_sim_not_pwdn=1 \
+    persist.vendor.radio.custom_ecc=1 \
+    persist.vendor.radio.rat_on=combine \
+    persist.vendor.radio.sib16_support=1
+
+# Sensors
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.sdk.sensors.gestures=false \
+    ro.vendor.sensors.cmc=false \
+    ro.vendor.sensors.dev_ori=true \
+    ro.vendor.sensors.facing=false \
+    ro.vendor.sensors.mot_detect=true \
+    ro.vendor.sensors.pmd=true \
+    ro.vendor.sensors.sta_detect=true
+
+# TimeService
+PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true
+
+# Wireless display
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.debug.wfd.enable=1 \
+    persist.sys.wfd.virtual=0
