b492fa038f04ce076b80330d1750b96cfbb039a3
Erik Castricum <git@cas-online.nl>
2019-09-25 09:04:52 +0200

    tone: simplify symlinks
    Signed-off-by: Erik Castricum <git@cas-online.nl>
    Change-Id: Iac566dfefe572d6c9953d0413beb4b10bf5df747

--- Android.mk
+++ Android.mk
@@ -27,140 +27,52 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(filter-out kagura kagura_ds,$(TARGET_DEVICE)),)
 
+# IMS links
+IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT_APPS_PRIVILEGED)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+$(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "IMS lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/lib64/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
+
+# RFS symlinks
+RFS_SYMLINKS := $(TARGET_OUT_VENDOR)/grfs
+$(RFS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "RFS links"
+	@rm -rf $(TARGET_OUT_VENDOR)/grfs
+	@mkdir -p $(TARGET_OUT_VENDOR)/grfs/apq/gnss/readonly
+	$(hide) ln -sf /persist/hlos_rfs/shared $(TARGET_OUT_VENDOR)/grfs/apq/gnss/hlos
+	$(hide) ln -sf /data/tombstones/lpass $(TARGET_OUT_VENDOR)/grfs/apq/gnss/ramdumps
+	$(hide) ln -sf /persist/rfs/apq/gnss $(TARGET_OUT_VENDOR)/grfs/apq/gnss/readwrite
+	$(hide) ln -sf /persist/rfs/shared $(TARGET_OUT_VENDOR)/grfs/apq/gnss/shared
+	$(hide) ln -sf /firmware $(TARGET_OUT_VENDOR)/grfs/apq/gnss/readonly/firmware
+	@mkdir -p $(TARGET_OUT_VENDOR)/grfs/msm/adsp/readonly
+	$(hide) ln -sf /persist/hlos_rfs/shared $(TARGET_OUT_VENDOR)/grfs/msm/adsp/hlos
+	$(hide) ln -sf /data/tombstones/lpass $(TARGET_OUT_VENDOR)/grfs/msm/adsp/ramdumps
+	$(hide) ln -sf /persist/rfs/msm/adsp $(TARGET_OUT_VENDOR)/grfs/msm/adsp/readwrite
+	$(hide) ln -sf /persist/rfs/shared $(TARGET_OUT_VENDOR)/grfs/msm/adsp/shared
+	$(hide) ln -sf /firmware $(TARGET_OUT_VENDOR)/grfs/msm/adsp/readonly/firmware
+	@mkdir -p $(TARGET_OUT_VENDOR)/grfs/msm/mpss/readonly
+	$(hide) ln -sf /persist/hlos_rfs/shared $(TARGET_OUT_VENDOR)/grfs/msm/mpss/hlos
+	$(hide) ln -sf /data/tombstones/lpass $(TARGET_OUT_VENDOR)/grfs/msm/mpss/ramdumps
+	$(hide) ln -sf /persist/rfs/msm/mpss $(TARGET_OUT_VENDOR)/grfs/msm/mpss/readwrite
+	$(hide) ln -sf /persist/rfs/shared $(TARGET_OUT_VENDOR)/grfs/msm/mpss/shared
+	$(hide) ln -sf /firmware $(TARGET_OUT_VENDOR)/grfs/msm/mpss/readonly/firmware
+
+ALL_DEFAULT_INSTALLED_MODULES += $(RFS_SYMLINKS)
+
+KEYMASTER_IMPL_SYMLINK := $(TARGET_OUT_VENDOR)/lib64/android.hardware.keymaster@3.0-impl-qti.so
+$(KEYMASTER_IMPL_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating keymaster impl symlink: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf hw/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(KEYMASTER_IMPL_SYMLINK)
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
-
-include $(CLEAR_VARS)
-
-RFS_APQ_GNSS_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/apq/gnss/
-$(RFS_APQ_GNSS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating RFS APQ GNSS folder structure: $@"
-	@rm -rf $@
-	@mkdir -p $(dir $@)/readonly
-	$(hide) ln -sf /data/vendor/tombstones/rfs/modem $@/ramdumpgns
-	$(hide) ln -sf /persist/rfs/apq/gnss $@/readwrite
-	$(hide) ln -sf /persist/rfs/shared $@/shared
-	$(hide) ln -sf /persist/hlos_rfs/shared $@/hlos
-	$(hide) ln -sf /firmware $@/readonly/firmware
-	$(hide) ln -sf /vendor/firmware $@/readonly/vendor
-
-RFS_MDM_ADSP_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/mdm/adsp/
-$(RFS_MDM_ADSP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating RFS MDM ADSP folder structure: $@"
-	@rm -rf $@
-	@mkdir -p $(dir $@)/readonly
-	$(hide) ln -sf /data/vendor/tombstones/rfs/lpass $@/ramdumps
-	$(hide) ln -sf /persist/rfs/mdm/adsp $@/readwrite
-	$(hide) ln -sf /persist/rfs/shared $@/shared
-	$(hide) ln -sf /persist/hlos_rfs/shared $@/hlos
-	$(hide) ln -sf /firmware $@/readonly/firmware
-	$(hide) ln -sf /vendor/firmware $@/readonly/vendor
-
-RFS_MDM_MPSS_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/mdm/mpss/
-$(RFS_MDM_MPSS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating RFS MDM MPSS folder structure: $@"
-	@rm -rf $@
-	@mkdir -p $(dir $@)/readonly
-	$(hide) ln -sf /data/vendor/tombstones/rfs/modem $@/ramdumps
-	$(hide) ln -sf /persist/rfs/mdm/mpss $@/readwrite
-	$(hide) ln -sf /persist/rfs/shared $@/shared
-	$(hide) ln -sf /persist/hlos_rfs/shared $@/hlos
-	$(hide) ln -sf /firmware $@/readonly/firmware
-	$(hide) ln -sf /vendor/firmware $@/readonly/vendor
-
-RFS_MDM_SLPI_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/mdm/slpi/
-$(RFS_MDM_SLPI_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating RFS MDM SLPI folder structure: $@"
-	@rm -rf $@
-	@mkdir -p $(dir $@)/readonly
-	$(hide) ln -sf /data/vendor/tombstones/rfs/slpi $@/ramdumps
-	$(hide) ln -sf /persist/rfs/mdm/slpi $@/readwrite
-	$(hide) ln -sf /persist/rfs/shared $@/shared
-	$(hide) ln -sf /persist/hlos_rfs/shared $@/hlos
-	$(hide) ln -sf /firmware $@/readonly/firmware
-	$(hide) ln -sf /vendor/firmware $@/readonly/vendor
-
-RFS_MDM_TN_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/mdm/tn/
-$(RFS_MDM_TN_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating RFS MDM TN folder structure: $@"
-	@rm -rf $@
-	@mkdir -p $(dir $@)/readonly
-	$(hide) ln -sf /data/vendor/tombstones/rfs/tn $@/ramdumps
-	$(hide) ln -sf /persist/rfs/mdm/tn $@/readwrite
-	$(hide) ln -sf /persist/rfs/shared $@/shared
-	$(hide) ln -sf /persist/hlos_rfs/shared $@/hlos
-	$(hide) ln -sf /firmware $@/readonly/firmware
-	$(hide) ln -sf /vendor/firmware $@/readonly/vendor
-
-RFS_MSM_ADSP_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/msm/adsp/
-$(RFS_MSM_ADSP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating RFS MSM ADSP folder structure: $@"
-	@rm -rf $@
-	@mkdir -p $(dir $@)/readonly
-	$(hide) ln -sf /data/vendor/tombstones/rfs/lpass $@/ramdumps
-	$(hide) ln -sf /persist/rfs/msm/adsp $@/readwrite
-	$(hide) ln -sf /persist/rfs/shared $@/shared
-	$(hide) ln -sf /persist/hlos_rfs/shared $@/hlos
-	$(hide) ln -sf /firmware $@/readonly/firmware
-	$(hide) ln -sf /vendor/firmware $@/readonly/vendor
-
-RFS_MSM_MPSS_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/msm/mpss/
-$(RFS_MSM_MPSS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating RFS MSM MPSS folder structure: $@"
-	@rm -rf $@
-	@mkdir -p $(dir $@)/readonly
-	$(hide) ln -sf /data/vendor/tombstones/rfs/modem $@/ramdumps
-	$(hide) ln -sf /persist/rfs/msm/mpss $@/readwrite
-	$(hide) ln -sf /persist/rfs/shared $@/shared
-	$(hide) ln -sf /persist/hlos_rfs/shared $@/hlos
-	$(hide) ln -sf /firmware $@/readonly/firmware
-	$(hide) ln -sf /vendor/firmware $@/readonly/vendor
-
-RFS_MSM_SLPI_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/msm/slpi/
-$(RFS_MSM_SLPI_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating RFS MSM SLPI folder structure: $@"
-	@rm -rf $@
-	@mkdir -p $(dir $@)/readonly
-	$(hide) ln -sf /data/vendor/tombstones/rfs/slpi $@/ramdumps
-	$(hide) ln -sf /persist/rfs/msm/slpi $@/readwrite
-	$(hide) ln -sf /persist/rfs/shared $@/shared
-	$(hide) ln -sf /persist/hlos_rfs/shared $@/hlos
-	$(hide) ln -sf /firmware $@/readonly/firmware
-	$(hide) ln -sf /vendor/firmware $@/readonly/vendor
-
-ALL_DEFAULT_INSTALLED_MODULES += $(RFS_APQ_GNSS_SYMLINKS) $(RFS_MDM_ADSP_SYMLINKS) $(RFS_MDM_MPSS_SYMLINKS) $(RFS_MDM_SLPI_SYMLINKS) $(RFS_MDM_TN_SYMLINKS) $(RFS_MSM_ADSP_SYMLINKS) $(RFS_MSM_MPSS_SYMLINKS) $(RFS_MSM_SLPI_SYMLINKS)
-
-WCNSS_INI_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini
-$(WCNSS_INI_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@echo "WCNSS config ini link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /vendor/etc/wifi/$(notdir $@) $@
-
-WCNSS_MAC_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/wlan_mac.bin
-$(WCNSS_MAC_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@echo "WCNSS MAC bin link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /data/vendor/wifi/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_INI_SYMLINK) $(WCNSS_MAC_SYMLINK)
-
-KEYMASTER_IMPL_SYMLINK := $(TARGET_OUT_VENDOR)/lib64/android.hardware.keymaster@3.0-impl-qti.so
-$(KEYMASTER_IMPL_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating keymaster impl symlink: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf hw/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(KEYMASTER_IMPL_SYMLINK)
-
-KEYMASTER_IMPL_SYMLINK := $(TARGET_OUT_VENDOR)/lib64/android.hardware.keymaster@3.0-impl-qti.so
-$(KEYMASTER_IMPL_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating keymaster impl symlink: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf hw/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(KEYMASTER_IMPL_SYMLINK)
 
 endif
