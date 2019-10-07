9eb53b8bc7e2033b8cc62123cfe39d683d595dda
Erik Castricum <git@cas-online.nl>
2019-10-07 15:11:44 +0200

    tone: android.hardware.radio.config@1.0
    needed by SODP boobs
    
    Signed-off-by: Erik Castricum <git@cas-online.nl>
    Change-Id: I49c7494058cedf9b85d444dc8bb23ae1dc65bdae

--- tone.mk
+++ tone.mk
@@ -311,6 +311,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.0 \
     android.hardware.radio@1.1 \
     android.hardware.radio@1.2 \
+    android.hardware.radio.config@1.0 \
     android.hardware.radio.deprecated@1.0 \
     libprotobuf-cpp-full \
     librmnetctl \
