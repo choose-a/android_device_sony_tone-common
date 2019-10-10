140dd35a73ca46ca21a6fb805c3c155f0f9a1e78
Erik Castricum <git@cas-online.nl>
2019-10-10 13:53:03 +0200

    [DONOT MERGE]adjust vendor properties
    Signed-off-by: Erik Castricum <git@cas-online.nl>
    Change-Id: Iea4fe4dbe4814f9a8f7673f6183a456ef85098da

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,28 +1,29 @@
-## Audio
+# Art
 PRODUCT_PROPERTY_OVERRIDES += \
-    audio.deep_buffer.media=true \
-    audio.offload.video=false \
+    ro.sys.fw.dex2oat_thread_count=4
+
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1 \
-    vendor.audio_hal.period_size=240 \
-    ro.qc.sdk.audio.fluencetype=none \
-    ro.qc.sdk.audio.ssr=false \
-    persist.audio.dualmic.config=endfire \
-    ro.vendor.audio.sdk.fluencetype=none \
+    vendor.audio_hal.period_size=192 \
+    ro.vendor.audio.sdk.fluencetype=fluence \
     persist.vendor.audio.fluence.voicecall=true \
     persist.vendor.audio.fluence.voicerec=false \
     persist.vendor.audio.fluence.speaker=true \
     vendor.audio.tunnel.encode=false \
     vendor.audio.offload.buffer.size.kb=64 \
-    vendor.audio.offload.pcm.16bit.enable=false \
-    vendor.audio.offload.pcm.24bit.enable=false \
+    vendor.audio.offload.pcm.16bit.enable=true \
+    vendor.audio.offload.pcm.24bit.enable=true \
+    audio.offload.video=false \
     vendor.audio.offload.track.enable=true \
-    vendor.voice.path.for.pcm.voip=false \
+    audio.deep_buffer.media=true \
+    vendor.voice.path.for.pcm.voip=true \
     vendor.audio.offload.multiaac.enable=true \
     vendor.audio.offload.multiple.enabled=false \
-    vendor.audio.offload.min.duration.secs=60 \
+    vendor.audio.offload.min.duration.secs=30 \
     vendor.audio.offload.passthrough=false \
     ro.vendor.audio.sdk.ssr=false \
-    vendor.audio.offload.gapless.enabled=false \
+    vendor.audio.offload.gapless.enabled=true \
     vendor.audio.safx.pbe.enabled=true \
     vendor.audio.parser.ip.buffer.size=262144 \
     vendor.audio.hw.aac.encoder=false \
@@ -31,146 +32,178 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.flac.sw.decoder.24bit=true \
     persist.vendor.bt.enable.splita2dp=false \
     persist.vendor.audio.speaker.prot.enable=true \
-    persist.vendor.audio.spkr.cal.duration=5 \
-    audio.offload.disable=false \
-    audio.playback.mch.downsample=0 \
-    persist.vendor.audio.fluence.mode=none \
-    persist.vendor.audio.endcall.delay=250 \
-    persist.vendor.audio.fluence.voicecomm=true
+    persist.vendor.audio.spkr.cal.duration=5
 
-## Bluetooth
+# Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     qcom.bluetooth.soc=rome \
     ro.bluetooth.emb_wp_mode=true \
     ro.bluetooth.wipower=true \
-    ro.bt.bdaddr_path=/data/vendor/bluetooth/bluetooth_bdaddr
-
-## Camera
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.HAL3.enabled=1 \
-    persist.camera.debug.logfile=0 \
-    persist.camera.gyro.disable=0 \
-    vendor.camera.aux.packagelist=org.codeaurora.snapcam,org.lineageos.snap \
-    persist.camera.camera2=true \
-    camera.disable_zsl_mode=1
-
-# Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=4m \
-    dalvik.vm.heapmaxfree=16m \
-    dalvik.vm.dex2oat-threads=2 \
-    dalvik.vm.image-dex2oat-threads=4 \
-
-# Data modules
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.data.iwlan.enable=true \
-    persist.data.mode=concurrent \
-    persist.data.netmgrd.qos.enable=true \
-    ro.use_data_netmgrd=true
-
-## CNE
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.cne.rat.wlan.chip.oem=nqc \
-    persist.cne.feature=1 \
-    persist.vendor.cne.feature=1
-
-# DRM
-PRODUCT_PROPERTY_OVERRIDES += \
-    drm.service.enabled=true
-
-## Graphics
-PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.gralloc.enable_fb_ubwc=1 \
-    debug.gralloc.enable_fb_ubwc=1 \
-    vendor.display.disable_skip_validate=1 \
-    sdm.debug.disable_skip_validate=1 \
-    dev.pm.dyn_samplingrate=1 \
-    persist.demo.hdmirotationlock=false \
-    persist.hwc.enable_vds=1 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.opengles.version=196610 \
-    debug.sf.recomputecrop=0 \
-    ro.sf.lcd_density=480
-
-# IMS
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.dbg.volte_avail_ovr=1 \
-    persist.dbg.vt_avail_ovr=1
-
-## Media
-PRODUCT_PROPERTY_OVERRIDES += \
-    media.aac_51_output_enabled=true \
-    mm.enable.qcom_parser=760765 \
-    vendor.mm.en.sec.smoothstreaming=false \
-    mm.enable.smoothstreaming=false \
-    mmp.enable.3g2=true \
-    vidc.enc.dcvs.extra-buff-count=2 \
-    vendor.vidc.enc.disable.pq=true
+    ro.bt.bdaddr_path=/data/vendor/bluetooth/bluetooth_bdaddr \
+    ro.vendor.bt.bdaddr_path=/data/vendor/bluetooth/bluetooth_bdaddr
 
 # NFC
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.nfc.port=I2C
 
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    vidc.debug.perf.mode=2 \
+    vidc.enc.dcvs.extra-buff-count=2 \
+    persist.audio.camcorder.stereo=true \
+    persist.camera.preview.ubwc=0 \
+    persist.camera.video.ubwc=0
+
+# CNE and DPM
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.cne.feature=1 \
+    persist.dpm.feature=0
+
+# Dalvik VM
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=4m \
+    dalvik.vm.heapmaxfree=8m \
+    dalvik.vm.heapgrowthlimit=256m
+
+# Data modules
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.use_data_netmgrd=true \
+    persist.data.df.dev_name=rmnet_usb0 \
+    persist.data.df.iwlan_mux=9 \
+    persist.data.iwlan.enable=true \
+    persist.data.mode=concurrent \
+    persist.data.netmgrd.qos.enable=true
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qualcomm.cabl=0 \
+    ro.sf.lcd_density=480
+
+# Fluence
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qc.sdk.audio.fluencetype=fluence \
+    persist.audio.fluence.voicecall=true \
+    persist.audio.fluence.voicerec=false \
+    persist.audio.fluence.speaker=true
+
+# GPS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.gps.qc_nlp_in_use=1 \
+    persist.loc.nlp_name=com.qualcomm.location \
+    ro.gps.agps_provider=1
+
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.hw=1 \
+    debug.egl.hw=1 \
+    debug.gralloc.enable_fb_ubwc=1 \
+    dev.pm.dyn_samplingrate=1 \
+    persist.demo.hdmirotationlock=false \
+    persist.sys.wfd.virtual=0 \
+    sdm.perf_hint_window=50 \
+    persist.hwc.enable_vds=1 \
+    sdm.debug.disable_rotator_split=1
+
+# HWUI
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=96 \
+    ro.hwui.layer_cache_size=64 \
+    ro.hwui.r_buffer_cache_size=12 \
+    ro.hwui.path_cache_size=39 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=7 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.text_small_cache_width=2048 \
+    ro.hwui.text_small_cache_height=2048 \
+    ro.hwui.text_large_cache_width=3072 \
+    ro.hwui.text_large_cache_height=2048
+
+# OpenGLES
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=196610
+
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.am.reschedule_service=true \
+    ro.min_freq_0=307200 \
+    ro.min_freq_4=307200 \
+    ro.sys.fw.bg_apps_limit=60 \
     ro.vendor.extension_library=libqti-perfd-client.so
 
-## Play Store
+# Play Store
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-sonymobile  \
     ro.com.google.clientidbase.ms=android-sonymobile
 
-## QCOM
+# RIL
 PRODUCT_PROPERTY_OVERRIDES += \
-    drm.service.enabled=true
-
-# Radio
-PRODUCT_PROPERTY_OVERRIDES += \
-    DEVICE_PROVISIONED=1 \
     rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
     vendor.rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
     ril.subscription.types=NV,RUIM \
+    DEVICE_PROVISIONED=1 \
+    persist.data.qmi.adb_logmask=0 \
+    persist.net.doxlat=true \
     persist.radio.apm_sim_not_pwdn=1 \
-    persist.vendor.radio.custom_ecc=1 \
-    persist.vendor.radio.rat_on=combine \
-    persist.vendor.radio.sib16_support=1 \
-    persist.vendor.radio.oem_socket=true \
-    persist.vendor.radio.add_power_save=1 \
-    persist.vendor.radio.wait_for_pbm=1 \
-    persist.vendor.radio.mt_sms_ack=19
+    persist.radio.custom_ecc=1 \
+    persist.radio.force_on_dc=true \
+    persist.radio.rat_on=combine \
+    persist.radio.redir_party_num=1 \
+    persist.radio.sib16_support=1 \
+    ro.telephony.call_ring.multiple=false
 
-# rmnet data
+# Radio - Data/RMNet
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.rmnet.data.enable=true \
-    persist.data.wda.enable=true \
-    persist.data.df.dl_mode=5 \
-    persist.data.df.ul_mode=5 \
     persist.data.df.agg.dl_pkt=10 \
     persist.data.df.agg.dl_size=4096 \
-    persist.data.df.mux_count=8 \
+    persist.data.df.dev_name=rmnet_usb0 \
+    persist.data.df.dl_mode=5 \
     persist.data.df.iwlan_mux=9 \
-    persist.data.df.dev_name=rmnet_usb0
+    persist.data.df.mux_count=8 \
+    persist.data.df.ul_mode=5 \
+    persist.data.iwlan.enable=true \
+    persist.data.qmi.adb_logmask=0 \
+    persist.data.wda.enable=true \
+    persist.rmnet.data.enable=true
+
+# Radio - IMS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.radio.NO_STAPA=1 \
+    persist.radio.VT_HYBRID_ENABLE=1
+
+# Security Patch Level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2017-11-16
 
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.sdk.sensors.gestures=false \
-    ro.vendor.sensors.cmc=false \
-    ro.vendor.sensors.dev_ori=true \
-    ro.vendor.sensors.facing=false \
-    ro.vendor.sensors.mot_detect=true \
-    ro.vendor.sensors.pmd=true \
-    ro.vendor.sensors.sta_detect=true
+    persist.debug.sensors.hal=e \
+    debug.qualcomm.sns.daemon=e \
+    debug.qualcomm.sns.libsensor1=e \
+    persist.sensors.lgpickup_en=true \
+    persist.sensors.mag_filter_size=8 \
+    persist.sensors.knock_delay=1000 \
+    persist.sensors.wul_multilevel=3 \
+    persist.sensors.wul_thresh0=2 \
+    persist.sensors.wul_thresh1=10 \
+    persist.sensors.wul_thresh2=15 \
+    persist.sensors.wul_delay=3000 \
+    persist.sensors.onhand.en=0
 
-# TimeService
+# Tethering
+PRODUCT_PROPERTY_OVERRIDES += \
+    net.tethering.noprovisioning=true
+
+# Time services
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true \
     persist.delta_time.enable=true
 
-# Wireless display
+# WiFi
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.debug.wfd.enable=1 \
-    persist.sys.wfd.virtual=0
+    wifi.interface=wlan0 \
+    ro.wifi.addr_path=/sys/devices/platform/soc/soc:bcmdhd_wlan/macaddr
