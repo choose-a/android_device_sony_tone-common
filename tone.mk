c06b17c5ed896e270526dac0fe54668674de1056
Erik Castricum <git@cas-online.nl>
2019-10-11 13:44:53 +0200

    tone: cleanup lineage remnants
    Signed-off-by: Erik Castricum <git@cas-online.nl>
    Change-Id: I0a28910ea9e966bc5036ff54f2bb49322ce00976

--- tone.mk
+++ tone.mk
@@ -242,10 +242,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.tone
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Mac address setup
 PRODUCT_PACKAGES += \
     macaddrsetup
@@ -351,10 +347,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
