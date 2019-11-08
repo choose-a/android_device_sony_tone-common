f3b95c9837315f7b2b40bba118d66659a69bf322
Erik Castricum <git@cas-online.nl>
2019-11-08 19:04:37 +0100

    Revert "nile-common: Update config.fs from stock"
    This reverts commit 66a84e563488480f5cd9a695b877f27eb1071fda.
    
    Signed-off-by: Erik Castricum <git@cas-online.nl>
    Change-Id: Ie2f7ab2ab48fc1260eb95a9f296c8df71f3cbb06

--- config.fs
+++ config.fs
@@ -1,44 +1,41 @@
-[AID_VENDOR_QTI_DIAG]
-value:2901
-
-[AID_VENDOR_QDSS]
-value:2902
+[AID_VENDOR_QCOM_DIAG]
+value: 2950
 
 [AID_VENDOR_RFS]
-value:2903
+value: 2951
 
 [AID_VENDOR_RFS_SHARED]
-value:2904
-
-[AID_VENDOR_ADPL_ODL]
-value:2905
-
-[AID_VENDOR_QRTR]
-value:2906
-
-[AID_VENDOR_ILLUMINATION]
-value:2929
+value: 2952
 
 [AID_VENDOR_QNS]
-value:2985
+value: 2985
 
 [AID_VENDOR_IDD]
-value:2987
+value: 2987
 
-[AID_VENDOR_SMIME_KEYSTORE]
-value:2992
+[AID_VENDOR_UPDATEMISCTA]
+value: 2991
 
 [AID_VENDOR_TRIMAREA]
-value:2993
+value: 2993
+
+[AID_VENDOR_PERSIST_RW]
+value: 2995
 
 [AID_VENDOR_CREDMGR_CLIENT]
-value:2996
+value: 2996
 
 [AID_VENDOR_TAD]
-value:2997
+value: 2997
 
 [AID_VENDOR_TA_QMI]
-value:2998
+value: 2998
+
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
 
 [bt_firmware/]
 mode: 0771
@@ -52,119 +49,17 @@ user: AID_MEDIA
 group: AID_MEDIA
 caps: 0
 
-[firmware/]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
-[firmware/image/*]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
 [persist/]
 mode: 0771
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: 0
 
-[system/bin/cnss-daemon]
-mode: 0755
-user: AID_BLUETOOTH
-group: AID_BLUETOOTH
-caps: NET_BIND_SERVICE
-
-[system/bin/pd-mapper]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[system/bin/pm-service]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[system/vendor/bin/cnd]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE NET_ADMIN BLOCK_SUSPEND
-
-[system/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_ADMIN BLOCK_SUSPEND
-
-[system/vendor/bin/ims_rtp_daemon]
-mode: 0755
-user: AID_SYSTEM
-group: AID_RADIO
-caps: NET_BIND_SERVICE
-
-[system/vendor/bin/imsdatadaemon]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[system/vendor/bin/imsrcsd]
-mode: 0755
-user: AID_SYSTEM
-group: AID_RADIO
-caps: NET_BIND_SERVICE WAKE_ALARM BLOCK_SUSPEND
-
-[system/vendor/bin/loc_launcher]
-mode: 0755
-user: AID_GPS
-group: AID_GPS
-caps: SETGID SETUID
-
-[system/vendor/bin/pd-mapper]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[system/vendor/bin/pm-service]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[system/vendor/bin/slim_daemon]
-mode: 0755
-user: AID_GPS
-group: AID_GPS
-caps: NET_BIND_SERVICE
-
-[system/vendor/bin/wcnss_filter]
-mode: 0755
-user: AID_BLUETOOTH
-group: AID_BLUETOOTH
-caps: BLOCK_SUSPEND
-
 [vendor/bin/cnd]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE NET_ADMIN BLOCK_SUSPEND
-
-[vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
-mode: 0755
-user: AID_BLUETOOTH
-group: AID_BLUETOOTH
-caps: NET_ADMIN BLOCK_SUSPEND
-
-[vendor/bin/ims_rtp_daemon]
-mode: 0755
-user: AID_SYSTEM
-group: AID_RADIO
-caps: NET_BIND_SERVICE
 
 [vendor/bin/imsdatadaemon]
 mode: 0755
@@ -175,14 +70,14 @@ caps: NET_BIND_SERVICE
 [vendor/bin/imsrcsd]
 mode: 0755
 user: AID_SYSTEM
-group: AID_RADIO
-caps: NET_BIND_SERVICE WAKE_ALARM BLOCK_SUSPEND
+group: AID_SYSTEM
+caps: WAKE_ALARM
 
-[vendor/bin/loc_launcher]
+[vendor/bin/ims_rtp_daemon]
 mode: 0755
-user: AID_GPS
-group: AID_GPS
-caps: SETGID SETUID
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE
 
 [vendor/bin/pd-mapper]
 mode: 0755
@@ -196,33 +91,20 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[vendor/bin/sensors.qti]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
 [vendor/bin/slim_daemon]
 mode: 0755
 user: AID_GPS
 group: AID_GPS
 caps: NET_BIND_SERVICE
 
-[vendor/bin/wcnss_filter]
-mode: 0755
-user: AID_BLUETOOTH
-group: AID_BLUETOOTH
-caps: BLOCK_SUSPEND
-
 [vendor/bin/xtwifi-client]
 mode: 0755
 user: AID_GPS
 group: AID_GPS
-caps: NET_BIND_SERVICE WAKE_ALARM BLOCK_SUSPEND
+caps: NET_BIND_SERVICE BLOCK_SUSPEND
 
-[vendor/firmware_mnt/image/*]
-mode: 0771
-user: AID_ROOT
-group: AID_SYSTEM
-caps: 0
-
+[vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: NET_ADMIN BLOCK_SUSPEND
